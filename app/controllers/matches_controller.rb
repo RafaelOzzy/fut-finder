42ad42586906b90023c90fa474d634578861e01b
Rafael Santos <rafael.ramos.s@gmail.com>
2023-10-03 16:07:45 -0300

    fix devise and some bugs from navbar

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -12,22 +12,49 @@ class MatchesController < ApplicationController
     end
   end
 
-  def create
-    @match = Match.new
-  end
-
   def new
     @match = Match.new
   end
+
+  def create
+    @match = Match.new(match_params)
+
+    if @match.save
+      redirect_to match_path(@match), notice: "Partida criada com sucesso!"
+    else
+      puts "Validation Errors: #{@match.errors.full_messages}"
+      render :new
+    end
+  end
+
 
   def show
     @match = Match.find(params[:id])
   end
 
+  def edit
+    @match = Match.find(params[:id])
+  end
+
+  def update
+    @match = Match.find(params[:id])
+    if match.update
+      redirect_to match_path(@match), notice: 'Partida atualizada com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+    redirect_to root_path, notice: 'Partida deletada com sucesso!'
+  end
+
   private
 
   def match_params
-    params.require(:match).permit(:address, :datetime, :latitude, :longitude)
+    params.require(:match).permit(:address, :datetime, :latitude, :longitude, :price, :bbq, :goalkeaper, :field_type)
   end
 
 
